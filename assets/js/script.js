ed0fe0306407f47f0c28ecb9889ae38d0b92faf6
Luke Skinner <laskinner@gmail.com>
2023-10-25 19:36:19 +0200

    Created some test text to ensure project is functioning

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+document.getElementById("welcome").textContent = "Welcome to hangman!";
+document.getElementById("instructions").textContent = "To play the game, just follow these simple instructions: ";
