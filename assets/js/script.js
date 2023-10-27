adefd96f532654507e34533a6a098a2cafa518d6
Luke Skinner <laskinner@gmail.com>
2023-10-27 02:42:51 +0200

    Created input and submit functionality, wired up comparison of letter array to letter guess

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 document.getElementById("welcome").textContent = "Welcome to hangman!";
 document.getElementById("instructions").textContent = "To play the game, just follow these simple instructions: ";
 
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("submit").addEventListener("click", function() {
+    checkAnswer();
+  });
+
+  document.getElementById("guess-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      checkAnswer();
+    }
+  });
+});
+
 const wordsArray = [
   "apple", "banana", "cherry", "date", "elder", "fig", "grape",
   "honey", "kiwi", "lemon", "mango", "nut", "olive", "pear",
@@ -35,13 +48,33 @@ console.log(letters);
 // Creates an object to track each letter as having been guessed or not
 const letterStatus = {};
 
-// Set each letter to default not guessed
+// Sets each letter to default not guessed
 letters.forEach(letter => {
   letterStatus[letter] = false;
 });
 
 console.log(letterStatus);
 
+function checkAnswer() {
+
+  let userAnswer = document.getElementById("guess-box").value;
+
+  // Converts user answer to lowercase if the words are provided in uppercase 
+  userAnswer = userAnswer.toLowerCase();
+
+  // Loops through letters to see if the guessed letter is in the word
+  letters.forEach(letter => {
+    if (userAnswer === letter) {
+      // Update the status of the guessed letter to true
+      letterStatus[letter] = true;
+    }
+  });
+
+  // Clears the input box after checking
+  document.getElementById("guess-box").value = "";
+
+  console.log(letterStatus);  // Logs updated letter status
+}
 
 
 
